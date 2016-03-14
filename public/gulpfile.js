ad8118d6534f6f9175074952ffb8446db6e09902
Jessica <jkoss0687@yahoo.com>
2016-03-14 10:23:33 -0400

    Added gulpfile info

--- public/gulpfile.js
+++ public/gulpfile.js
@@ -1,1 +1,31 @@
+var gulp = require('gulp');
+var browserSync = require('browser-sync').create();
+var karma = require('karma').Server; 
+var jshint = require('gulp-jshint');
 
+gulp.task('test', function(done){   
+	new karma({     configFile: __dirname + '/karma.conf.js'   
+	}, done).start(); 
+});
+
+gulp.task('lint', function() {
+  return gulp.src('*.js')
+    .pipe(jshint())
+    .pipe(jshint.reporter('default'));
+});
+
+
+gulp.task('browser-sync', function() {
+    browserSync.init({
+        server: {
+            baseDir: "./"
+        }
+    });
+});
+
+
+gulp.task('one', function () {
+  return gulp.src('./css/one.styl')
+    .pipe(stylus())
+    .pipe(gulp.dest('./css/build'));
+});
