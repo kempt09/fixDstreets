3eda80cd4d5b901b6e3eb60e43434d43d300b0ce
christopher <kempt09@gmail.com>
2016-03-15 17:05:08 -0400

    fix small issue with controllers

--- public/js/controllers/controllers.js
+++ public/js/controllers/controllers.js
@@ -1,28 +1,23 @@
 var app = app || {};
 
-app.controller('submitIssue', ['$scope', function($scope){
+app.controller('submitIssue', ['$scope', '$http', function($scope, $http){
     'use strict';
-
-}]);
-
-app.controller('mapbox', [function(){
-    'use strict';
-    mapboxgl.accessToken = 'pk.eyJ1IjoiZml4ZHN0cmVldHMiLCJhIjoiY2lsczNxMHYxMDhzNXZmbHlmbWdkM2psaiJ9.0HF3gwCxpsc_s2d8HxvXwg';
-    var bounds = [
-        [-83.03841204223656, 42.32883494037918],
-        [-83.06291670379659, 42.344030329635274]
-    ];
-    var map = new mapboxgl.Map({
-        container: 'map', // container id
-        style: 'mapbox://styles/fixdstreets/cilse2gcq009va2kqe2eqzzgo', //stylesheet location
-        center: [-83.0506, 42.3361], // starting position
-        zoom: 14, // starting zoom
-        interactive: true,
-        maxBounds: bounds
-    });
-    map.on('click', function (e) {
-        document.getElementById('info').innerHTML = e.lngLat;
+    $scope.lat = '';
+    $scope.long = '';
+    $scope.description = '';
+    $scope.image = '';
+    $scope.postTicket = $http({
+        method: 'POST',
+        url: '/api/submit',
+        data: {
+            lat: $scope.lat,
+            long: $scope.long,
+            description: $scope.description,
+            image: $scope.image
+        }
     });
 }]);
+
+
 
 
