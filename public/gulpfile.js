4c1c245ccd0bcdde61dd02ee687b05247431dcbd
Jessica <jkoss0687@yahoo.com>
2016-03-17 13:54:49 -0400

    added gulp files, worked on homepage

--- public/gulpfile.js
+++ public/gulpfile.js
@@ -1,14 +1,27 @@
-var gulp = require('gulp');
+var gulp        = require('gulp');
 var browserSync = require('browser-sync').create();
-var jshint = require('gulp-jshint');
+var jshint      = require('gulp-jshint');
+    karma       = require('karma').Server;
+var stylus      = require('gulp-stylus');
 
+//Default task
+gulp.task('default', ['watch']);
+
+//test with Karma
+gulp.task('test', function(done){
+  new karma({
+    configFile: __dirname + '/karma.conf.js'
+  }, done).start();
+});
+
+//jshint
 gulp.task('lint', function() {
-  return gulp.src('*.js')
+  return gulp.src('./js/**/*.js')
     .pipe(jshint())
     .pipe(jshint.reporter('default'));
 });
 
-
+//browser-sync
 gulp.task('browser-sync', function() {
     browserSync.init({
         server: {
@@ -17,8 +30,15 @@ gulp.task('browser-sync', function() {
     });
 });
 
+//Compile in stylus
 gulp.task('style', function () {
-  return gulp.src('./stylesheets/style.styl')
+  return gulp.src('./stylesheets/style.stylus')
     .pipe(stylus())
     .pipe(gulp.dest('./stylesheets/build'));
+});
+
+//Watch files for changes
+gulp.task('watch', function(){
+  gulp.watch('./stylesheets/style.stylus', ['stylus']);
+  gulp.watch('./js/**/*.js', ['jshint', 'build-js']);
 });
