52795a849175c6e4cd432fbc4fd4c7f8b0323268
christopher <kempt09@gmail.com>
2016-03-22 14:00:43 -0400

    fix conflicts

--- public/js/controllers/controllers.js
+++ public/js/controllers/controllers.js
@@ -25,14 +25,13 @@ app.controller('submitTicket', ['$scope', '$http', 'dbCollection', function ($sc
 
 app.controller('ticketFeed', ['$scope', 'getTickets', 'dbCollection', function ($scope, getTickets, dbCollection) {
     'use strict';
-        $scope.auth = function(googleUser){
-         dbCollection.profile = googleUser.getBasicProfile();
-          console.log('ID: ' + profile.getId()); // Do not send to your backend! Use an ID token instead.
-          console.log('Name: ' + profile.getName());
-          console.log('Image URL: ' + profile.getImageUrl());
-          console.log('Email: ' + profile.getEmail());
-        }
-        
+    $scope.auth = function(googleUser){
+        dbCollection.profile = googleUser.getBasicProfile();
+        console.log('ID: ' + profile.getId()); // Do not send to your backend! Use an ID token instead.
+        console.log('Name: ' + profile.getName());
+        console.log('Image URL: ' + profile.getImageUrl());
+        console.log('Email: ' + profile.getEmail());
+    };
     getTickets.then(function(response){
         $scope.collection = response;
         console.log(response);
