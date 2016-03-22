9dcf557e13a036d8623343510b10e0290f544c75
Chrisopher Kempton <kempt09@gmail.com>
2016-03-22 19:57:39 -0400

    Update api.js

--- routes/api.js
+++ routes/api.js
@@ -7,11 +7,6 @@ var S3FS = require('s3fs');
 var multiparty = require('connect-multiparty');
 var multipartyMiddware = multiparty();
 
-// Amazon S3 Credentials
-var s3fsUploads = new S3FS('fixdstreets',{
-    accessKeyId: "AKIAI7OUBEY6DSO73ODA",
-    secretAccessKey: "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"
-});
 
 // Initialize S3 bucket for files
 s3fsUploads.create();
